8206c7ae7d84e81ccc1a9924e866dd37f2395681
pygosuperman <pygosuperman@outlook.com>
2021-12-19 00:54:14 +0800

    添加sudo命令

--- examples/z07_command_sudo/main.go
+++ examples/z07_command_sudo/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	s := zdpgo_ssh.New("192.168.18.13", "zhangdapeng", "zhangdapeng", 22)
-	output, err := s.Sudo("ls -lah")
+	s := zdpgo_ssh.New("192.168.18.101", "zhangdapeng", "zhangdapeng", 22)
+	output, err := s.Sudo("ls")
 	fmt.Printf("%v\n%v", output, err)
 }
